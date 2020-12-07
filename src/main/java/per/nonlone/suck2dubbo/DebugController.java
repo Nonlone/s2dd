00c71539c7636a98c7ed27076ac95459187411a5
Nonlone <thunderbird.shun@gmail.com>
2020-12-08 02:47:21 +0800

    feat:增加nacos发现

--- src/main/java/per/nonlone/suck2dubbo/DebugController.java
+++ src/main/java/per/nonlone/suck2dubbo/DebugController.java
@@ -3,6 +3,8 @@ package per.nonlone.suck2dubbo;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.rpc.service.GenericService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class DebugController {
 
     private static final String DUBBO_PROTOCOL = "dubbo";
+
+    private static final String NACOS_PROTOCOL = "nacos";
+
 
     @GetMapping("index")
     public Object index() {
@@ -45,11 +50,57 @@ public class DebugController {
         @RequestBody Map<String,Object> data
     ) {
 
-        DebugReferenceConfig<GenericService> reference = new DebugReferenceConfig<GenericService>();
+        ReferenceConfig<GenericService> reference = new ReferenceConfig<GenericService>();
         reference.setGeneric(true);
         reference.setInterface(classOfService);
         reference.setVersion(version);
         reference.setUrl(DUBBO_PROTOCOL+"://"+ip+":"+port);
+        reference.setProtocol(DUBBO_PROTOCOL);
+        if(StringUtils.isNotBlank(version)){
+            reference.setVersion(version);
+        }
+        if(StringUtils.isNotBlank(group)){
+            reference.setGroup(group);
+        }
+        GenericService genericService = reference.get();
+        return genericService.$invoke(metchod,new String[]{classOfRequest},new Object[]{data});
+    }
+
+
+    /**
+     * 连接Nacos调试
+     * @param namespace
+     * @param ip
+     * @param port
+     * @param classOfService
+     * @param metchod
+     * @param version
+     * @param group
+     * @param classOfRequest
+     * @param data
+     * @return
+     */
+    @PostMapping("dwn/{namespace}/{ip}/{port}/{class}/{method}")
+    public Object debugWithNacos(
+        @PathVariable("namespace")String namespace,
+        @PathVariable("ip") String ip,
+        @PathVariable("port") String port,
+        @PathVariable("class") String classOfService,
+        @PathVariable("method") String metchod,
+        @RequestHeader(value = "version",defaultValue = "")String version,
+        @RequestHeader(value = "group",defaultValue = "")String group,
+        @RequestHeader(value = "class")String classOfRequest,
+        @RequestBody Map<String,Object> data
+    ) {
+        RegistryConfig registryConfig=new RegistryConfig();
+        registryConfig.setAddress(NACOS_PROTOCOL+"://"+ip+":"+port+"?namespace="+namespace);
+
+        ReferenceConfig<GenericService> reference = new ReferenceConfig<GenericService>();
+        reference.setGeneric(true);
+        reference.setRegistry(registryConfig);
+        reference.setInterface(classOfService);
+        reference.setVersion(version);
+        // reference.setUrl(DUBBO_PROTOCOL+"://"+ip+":"+port);
         reference.setProtocol(DUBBO_PROTOCOL);
         if(StringUtils.isNotBlank(version)){
             reference.setVersion(version);
